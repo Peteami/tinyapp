0434f9a0f4820a8e21e53eb10f8091f6a5202ceb
Peteami <peteami@hotmail.com>
2020-09-04 01:43:59 +0000

    refactor with cookie session and access to own resources

--- express_server.js
+++ express_server.js
@@ -7,13 +7,19 @@ app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-const cookieParser = require("cookie-parser");
-app.use(cookieParser());
+// const cookieParser = require("cookie-parser");
+// app.use(cookieParser());
+
+const cookieSession = require("cookie-session");
+const { request } = require("express");
+app.use(cookieSession({
+  name: "session",
+  keys: ["key1", "key2"]
+}));
 
 
 
-// -------------FUNCTIONS---------------------
-
+// -----------------------FUNCTIONS---------------------------------
 function generateRandomString() {
   let number = Math.random().toString(36).substr(2, 6);
   return number;
@@ -41,12 +47,26 @@ function checkMatchingEmailPassword(users, password) {
   }
   return false;
 }
-// ------------------------------------------------------------
+
+
+function isPersonalUrl(database, id) {
+  let personalUrls = {};
+  for (let data in database) {
+    if (database[data].userID === id) {
+      personalUrls[data] = database[data];
+    }
+  }
+  return personalUrls;
+}
+// -----------------------------------------------------------------
+
 
 
 const urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "b2xVn2": { longURL: "http://www.lighthouselabs.ca", userID: "userRandomID"},
+  "9sm5xK": { longURL: "http://www.google.com", userID: "userRandomID"},
+  "c8Tm5c": { longURL: "http://www.cnet.com", userID: "user2RandomID"},
+  "k6Tf9V": { longURL: "http://www.tsn.ca", userID: "user"}
 };
 
 const users = {
@@ -59,14 +79,31 @@ const users = {
     id: "user2RandomID",
     email: "user2@example.com",
     password: "dishwasher-funk"
-  }
+  },
+  "user": {
+    id: "user",
+    email: "user@user.com",
+    password: "user"
+  },
 };
 
 
+
+//------------------------Home route-------------------------
+app.get("/", (req, res) => {
+  res.redirect("/login");
+});
+// -------------------------------------------------------------
+
+
 // ---------------Login With email and Password--------------
-// --------------------------------------------------------------
 app.get("/login", (req, res) => {
-  let templateVars = { urls: urlDatabase, user: users[req.cookies["username"]] };
+  let templateVars = { 
+    user: req.session.user_id
+  };
+  if (req.session.user_id) {
+    res.redirect("/urls")
+  }
   res.render("login", templateVars);
 });
 
@@ -76,27 +113,29 @@ const email = req.body.email;
 const password = req.body.password;
 const user = checkForExistingEmail(users, email);
 if (user && user.password === password){
-res.cookie('user_id', user.id);
-res.redirect("/urls")
+  console.log("user------->>>>>", user)
+  req.session.user_id = user.id;
+  res.redirect("/urls")
 } else {
-    res.sendStatus(403);
+    res.redirect("/register");
   }
 });
 // -----------------------------------------------------------------
 
 
 
-// Logout
+// -----------------------------Logout Route---------------------------
 app.post("/logout", (req, res) => {
-  res.clearCookie("user_id");
+  req.session.user_id = null;
   res.redirect("/login");
 });
+// ----------------------------------------------------------------------
 
 
-// -------------------------------------
-// Register
+
+// ---------------------Register Route------------------------------
 app.get("/register", (req, res) => {
-  let templateVars = { urls: urlDatabase, user: users[req.cookies["username"]] };
+  let templateVars = { user: users[req.session.user_id] };
   res.render("register", templateVars);
 });
 
@@ -115,102 +154,154 @@ app.post("/register", (req, res) => {
       email: email,
       password: password
     }
-    res.cookie("user_id", id);
+
     res.redirect("/urls");
   }
 });
-
-// app.get('/register', (req, res) => {
-//   res.render('register');
-// });
-// ------------------------------------------
+// -------------------------------------------------------------------
 
 
-// Home route
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
 
-
+// ----------------------See All Urls Route-------------------------------
 // Route to see all urls
 app.get("/urls", (req, res) => {
-  const cookieId = req.cookies["user_id"]
-  let templateVars = { urls: urlDatabase, user: users[cookieId] };
-  console.log("test TempVara", templateVars)
-  console.log("test CookieId", cookieId)
-  res.render("urls_index", templateVars);
+  let cookieId = req.session["user_id"]
+  if (!cookieId) {
+    res.redirect("/login")
+  } else {
+    let templateVars = { 
+      urls: isPersonalUrl(urlDatabase, cookieId),  //here to change with function
+      user: users[cookieId] };
+    res.render("urls_index", templateVars);
+  }
 });
 
 
+// app.get("/urls", (req, res) => {
+// const cookieId = req.session["user_id"]
+// if (!cookieId) {
+//   res.redirect("/login")
+// } else {
+//   let templateVars = { urls: urlDatabase, user: users[cookieId] };
+//   res.render("urls_index", templateVars);
+// }
+// });
 
-//  --------------------Create new short url get and post----------------
+app.post("/urls", (req, res) => {
+  let cookieId = req.session["user_id"]
+  if (!cookieId) {
+    res.redirect("/login")
+  } else {
+    let shortURL = generateRandomString();
+    urlDatabase[shortURL] = {longURL: req.body.longURL, userID: cookieId}
+    // console.log(urlDatabase);
+    res.redirect(`urls/${shortURL}`);
+  }
+});
+// --------------------------------------------------------------------------
+
+
+
+//  --------------------Create new short url Route------------------------
+
+
+app.get("/urls/new", (req, res) => {
+  if (users[req.session.user_id]) {
+    let templateVars = { 
+      user: users[req.session.user_id]
+    }
+    res.render("urls_new", templateVars);
+  } else {
+    res.redirect("/login");
+  }
+})
+
+
+//-----new
+app.post("/urls", (req, res) => {
+  let cookieId = req.session["user_id"]
+  const shortString = generateRandomString();
+  urlDatabase[shortString] = {
+    longURL: req.body.longURL,
+    userID: cookieId
+  }
+  res.redirect(`/urls/${shortString}`)
+})
+// ------------------------------------------------------------------------
+
+
+
+
+// ---------------------Modify an url Route------------------------------
+app.get("/urls/:shortURL/modify", (req, res) => {
+  res.redirect(`/urls/${req.params.shortURL}`);
+});
+
+app.post("/urls/:shortURL/modify", (req, res) => {
+  // console.log(req.body);
+  // console.log(urlDatabase);
+  urlDatabase[req.params.shortURL] = req.body.longURL;
+  // console.log(urlDatabase);
+  res.redirect(`/urls`);
+});
 // ----------------------------------------------------------------------
 
-// Route to page form to create a new short url
-app.get("/urls/new", (req, res) => {
-  const cookieId = req.cookies["username"]
-  let templateVars = { urls: urlDatabase, user: users[cookieId] };
-  res.render("urls_new", templateVars);
+
+
+//-----------------------Delete an url Route----------------------------
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
 });
-
-// Create new short url and redirect to the page from this new url
-app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  let randomString = generateRandomString();
-  urlDatabase[randomString] = req.body.longURL;
-  console.log(urlDatabase);
-  // res.send("Ok");         // Respond with 'Ok' (we will replace this)
-  res.redirect(`urls/${randomString}`);
-});
-// --------------------------------------------------------------------
+// ---------------------------------------------------------------------
 
 
 
-// Route to get specific long url info with the short url info
+
+//---------------------Show specific long url Route------------------------
 //1st shortURL is linked to the show.ejs file
 app.get("/urls/:shortURL", (req, res) => {
   //req.params is linked to the app.get url name
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL], urls: urlDatabase, user: users[req.cookies["username"]]};
-  res.render("urls_show", templateVars);
-  console.log(req.params);
+  if (users[req.session.user_id]) {
+    let url = urlDatabase[req.params.shortURL]
+    if (req.session.user_id === url.userID) {
+        let templateVars = { 
+        shortURL: req.params.shortURL,
+        longURL: url.longURL, 
+        user: users[req.session.user_id]
+      };
+      return res.render("urls_show", templateVars);
+    } else {
+      return res.status(403).send("Unautorize access");
+    }
+  } else {
+    return res.status(403).send("You are not logged in")
+  }
 });
+//--------------------------------------------------------------------------
+
+
+
+
+//---------------------------Say Hello Route----------------------------
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+//----------------------------------------------------------------------
+
 
 
 app.get("/u/:shortURL", (req, res) => {
-  res.redirect(urlDatabase[req.params.shortURL]);
+  // console.log(urlDatabase[req.params.shortURL].longURL);
+  res.redirect(urlDatabase[req.params.shortURL].longURL);
 });
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
-// Route to /hello
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
 
 
-// Delete an url
-app.post("/urls/:shortURL/delete", (req, res) => {
-  delete urlDatabase[req.params.shortURL];
-  res.redirect("/urls");
-});
-
-
-// ---------------------------------------------------
-// Modify an url
-app.get("/urls/:shortURL/modify", (req, res) => {
-  res.redirect(`/urls/${req.params.shortURL}`);
-});
-
-app.post("/urls/:shortURL/modify", (req, res) => {
-  console.log(req.body);
-  console.log(urlDatabase);
-  urlDatabase[req.params.shortURL] = req.body.longURL;
-  console.log(urlDatabase);
-  res.redirect(`/urls`);
-});
-// ------------------------------------------------------
 
 
 // Start server on port with message
